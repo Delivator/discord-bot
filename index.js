0d2526b64184aa50ef8a5275bb5038a004a0cbcb
David <delivatorgaming@gmail.com>
2017-07-18 22:33:20 +0200

    Basic command functionality

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ client.on("ready", () => {
   console.log(`Bot ready! Logged in as ${client.user.tag}\\${client.user.id}`);
 });
 
+client.on("message", message => {
+  var author = message.author,
+      channel = message.channel,
+      content = message.conten,
+      guild = message.guild;
+
+  if (conten.startsWith(settings.cmd_prefix) && author != client.user) {
+    var cmd = conten.substring(settings.cmd_prefix.length).split(" ")[0];
+    console.log(cmd);
+  }
+});
+
 client.login(settings.token);
