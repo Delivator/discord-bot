f56c6e80c2c0fab9a55f92c9df63a9cb58b6ab4e
David <delivatorgaming@gmail.com>
2017-07-26 03:10:52 +0200

    Small updated for the play command

--- commands/play.js
+++ commands/play.js
@@ -3,16 +3,17 @@ const musicPlayer = require("../util/musicPlayer");
 
 exports.run = (client, message, args) => {
   if (!args[0]) {
-    message.channel.send(`Please provide a link! Use \`${settings.prefix}help play\` for more information.`);
+    message.channel.send(`[Music] Please provide a link! Use \`${settings.prefix}help play\` for more information.`);
   } else {
     if (!message.member.voiceChannel) {
-      message.channel.send("You have to be in a voice channel to use this command.");
+      message.channel.send("[Music] You have to be in a voice channel to use this command.");
     } else {
       if (!musicPlayer.servers[message.guild.id]) musicPlayer.servers[message.guild.id] = {
         queue: []
       };
       let server = musicPlayer.servers[message.guild.id];
       server.queue.push(args[0]);
+      message.channel.send(`[Music] \`${args[0]}\` has been added to the queue.`);
       if (!message.guild.voiceConnection) message.member.voiceChannel.join()
         .then(connection => {
           musicPlayer.play(connection, message);
