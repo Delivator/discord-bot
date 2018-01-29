64629154b58e97d62e489c2dcb8da56e34274556
Delivator <delivatorgaming@gmail.com>
2018-01-29 20:36:06 +0100

    v1.8.4
    - Updated modules
    Switched from youtube-dl to my own module called youtube-dl.js

--- util/musicDownloader.js
+++ util/musicDownloader.js
@@ -1,31 +1,36 @@
 const fs = require("fs");
-const youtubedl = require("youtube-dl");
+// const youtubedl = require("youtube-dl");
+const ytdl = require("youtube-dl.js");
 const crypto = require("crypto");
 const log = require("../util/logFunction").log;
 
 exports.downloadSong = (url, isYT) => {
   return new Promise(function(resolve, reject) {
-    let video;
-    let fileName = crypto.createHash("md5").update(url).digest("hex");
+    let fileName = crypto.createHash("md5").update(url).digest("hex"),
+        ytdlArgs,
+        tempFilename = new Date().getTime();
+
     if (fs.existsSync(`./.cache/${fileName}`)) return resolve(fileName);
     if (isYT) {
-      video = youtubedl(url, ["--extract-audio", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k", "--format=bestaudio"], { cwd: __dirname, maxBuffer: 1000*1024 });
+      ytdlArgs = ["-o", `../.cache/${tempFilename}.%(ext)s`, "-x", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"];
     } else {
-      video = youtubedl(url, ["--extract-audio", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"], { cwd: __dirname, maxBuffer: 1000*1024 });
+      ytdlArgs = ["-o", `../.cache/${tempFilename}.%(ext)s`, "-x", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"]
     }
-    video.pipe(fs.createWriteStream(`./.cache/${fileName}`));
+
     log(`[YTDL] Downloading file "${url}" to ".cache/${fileName}"`);
-    video.on("end", function() {
-      let fileSize = fs.statSync(`./.cache/${fileName}`)["size"] / 1048576;
-      log(`[YTDL] Download of "${fileName}" finished! Filesize: ${String(fileSize).substring(0, 4)} MiB`);
-      resolve(fileName);
-    });
-    video.on("error", function(err) {
-      log("[YTDL] Couldn't download file. Deleting empty file.");
-      let newFileName = `del_${new Date().getTime()}`;
-      fs.renameSync(`./.cache/${fileName}`, `./.cache/${newFileName}`);
-      fs.unlinkSync(`./.cache/${newFileName}`);
-      reject(err);
-    });
+    ytdl(url, ytdlArgs, { cwd: __dirname, maxBuffer: 1000 * 1024 })
+      .then(data => {
+        fs.rename(`./.cache/${tempFilename}.mp3`, `./.cache/${fileName}`, err => {
+          if (err) return log(err);
+          let fileSize = fs.statSync(`./.cache/${fileName}`)["size"] / 1048576;
+          log(`[YTDL] Download of "${fileName}" finished! Filesize: ${String(fileSize).substring(0, 4)} MiB`);
+          resolve(fileName);
+        });
+      })
+      .catch(err => {
+        log("[YTDL] Couldn't download file.");
+        log(err);
+        reject(err);
+      });
   });
 };
